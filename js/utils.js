f4460ebabe441a98859b6720d26a8c9a2c804425
Yooooui <3052915674@qq.com>
2026-07-01 23:23:19 +0800

    Site updated: 2026-07-01 23:23:19

--- js/utils.js
+++ js/utils.js
@@ -554,8 +554,7 @@ KEEP.initUtils = () => {
           script.setAttribute('data-pjax', '')
           script.setAttribute('id', tmpId)
           script.async = true
-          // script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
-          script.src = '//events.vercount.one/js'
+          script.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
           document.body.appendChild(script)
         }
 
